cc09b44c7ece498a1dabd99742bb4f831d006784
lishuangxi <lishuangxi@vv.cc>
2018-04-03 09:11:56 +0800

    update entity

--- botong-info/src/main/java/com/yunjing/info/model/InfoContent.java
+++ botong-info/src/main/java/com/yunjing/info/model/InfoContent.java
@@ -12,6 +12,24 @@ import lombok.Data;
 @Data
 @TableName("info_content")
 public class InfoContent extends BaseModel<InfoContent> {
+    /**
+     * CREATE TABLE `info_content` (
+     `id` bigint(22) NOT NULL COMMENT '主键id',
+     `catalog_id` bigint(22) DEFAULT NULL COMMENT '目录id',
+     `org_id` bigint(22) DEFAULT NULL COMMENT '公司id',
+     `department_name` bigint(22) DEFAULT NULL COMMENT '部门id',
+     `title` varchar(35) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
+     `picture_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图片地址',
+     `content` longtext COLLATE utf8mb4_unicode_ci COMMENT '内容',
+     `read_number` int(11) DEFAULT '0' COMMENT '阅读数量',
+     `whether_show` tinyint(1) DEFAULT '1' COMMENT '是否显示0：否，1：是',
+     `sort` tinyint(6) DEFAULT '0' COMMENT '展示顺序',
+     `is_delete` tinyint(1) DEFAULT NULL COMMENT '是否删除 0：未删除；1：已删除',
+     `create_time` bigint(20) DEFAULT NULL COMMENT '创建时间',
+     `update_time` bigint(20) DEFAULT NULL COMMENT '修改时间',
+     PRIMARY KEY (`id`)
+     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='资讯内容';
+     */
 
     /**
      * 目录id
@@ -26,8 +44,15 @@ public class InfoContent extends BaseModel<InfoContent> {
     private Long orgId;
 
     /**
+     * 部门名称
+     */
+    @TableField("department_name")
+    private String departmentName;
+
+    /**
      * 标题
      */
+    @TableField("title")
     private String title;
 
     /**
@@ -39,7 +64,13 @@ public class InfoContent extends BaseModel<InfoContent> {
     /**
      * 内容
      */
+    @TableField("content")
     private String content;
+    /**
+     *
+     */
+    @TableField("read_number")
+    private Integer readNumber;
 
     /**
      * 是否轮播 0：否，1：是
@@ -50,6 +81,7 @@ public class InfoContent extends BaseModel<InfoContent> {
     /**
      * 展示顺序
      */
+    @TableField("sort")
     private Integer sort;
 
     /**
