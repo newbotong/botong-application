d60ee1b586f326d5aadb69ada03e8df4759b8005
015 <liudongbo@vv.cc>
2018-04-03 10:01:48 +0800

    添加日志常量

---
+++ botong-log/src/main/java/com/yunjing/botong/log/constant/LogConstant.java
@@ -0,0 +1,24 @@
+package com.yunjing.botong.log.constant;
+
+import java.io.Serializable;
+
+/**
+ * 签到中常量
+ * @version 1.0.0
+ * @author: jingwj
+ * @date 2018/3/28 17:07
+ * @description
+ **/
+public class LogConstant implements Serializable {
+
+    public final static String  SEPARATE_STR = ",";
+    public final static String  SEPARATE_STR_SPACE = " ";
+
+    public final static String  CODE_STR = "code";
+    public final static String  MSG_STR = "msg";
+    public final static String  DATA_STR = "data";
+
+    public final static String  CODE_SCCUESS = "200";
+
+
+}
