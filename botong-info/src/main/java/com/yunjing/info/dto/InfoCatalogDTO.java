4c39bc6b157ca0e7e29e98d867d2320ee566e849
VDESKTOP\043 <liushujie@vv.cc>
2018-04-04 17:42:15 +0800

    资讯web端接口编写

---
+++ botong-info/src/main/java/com/yunjing/info/dto/InfoCatalogDTO.java
@@ -0,0 +1,58 @@
+package com.yunjing.info.dto;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.common.mybatis.model.BaseModel;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 资讯目录实体
+ *
+ * @author 裴志鹏
+ * @date 2018/3/20 17:07
+ */
+@Data
+public class InfoCatalogDTO extends BaseModel<InfoCatalogDTO> {
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 机构id
+     */
+    private Long orgId;
+
+    /**
+     * 父id
+     */
+    private Long parentId;
+
+    /**
+     * 级别
+     */
+    private Integer level;
+
+    /**
+     * 展示顺序
+     */
+    private Integer sort;
+
+    /**
+     * 是否删除 0：未删除；1：已删除
+     */
+    private Integer isDelete;
+
+    /**
+     * 是否显示0：否，1：是
+     */
+    private Integer whetherShow;
+
+    /**
+     * 子集
+     */
+    private List<InfoCatalogDTO> lower;
+}
