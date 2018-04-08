a183589097062a7fa0d44dd2c960f246c5143b6a
lishuangxi <lishuangxi@vv.cc>
2018-04-08 13:44:48 +0800

    update

---
+++ botong-info/src/main/java/com/yunjing/info/config/JsonConfig.java
@@ -0,0 +1,14 @@
+package com.yunjing.info.config;
+
+import com.yunjing.mommon.config.JacksonConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Long类型返回值Json转换String配置
+ *
+ * @author 李双喜
+ * @date 2018/3/28 13:45
+ */
+@Configuration
+public class JsonConfig extends JacksonConfig {
+}
