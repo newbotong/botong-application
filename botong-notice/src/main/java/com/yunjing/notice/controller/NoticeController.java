4ccea72942b41fc82125eca31b4068afd960d490
lishuangxi <lishuangxi@vv.cc>
2018-04-12 10:12:15 +0800

    update

--- botong-notice/src/main/java/com/yunjing/notice/controller/NoticeController.java
+++ botong-notice/src/main/java/com/yunjing/notice/controller/NoticeController.java
@@ -6,9 +6,11 @@ import com.yunjing.mommon.wrapper.ResponseEntityWrapper;
 import com.yunjing.notice.body.NoticeBody;
 import com.yunjing.notice.body.NoticeDetailsBody;
 import com.yunjing.notice.service.NoticeService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -25,13 +27,14 @@ public class NoticeController extends BaseController {
     private NoticeService noticeService;
 
     /**
-     * 新增公告接口
+     * 新增公告
      *
-     * @param body 入参实体
-     * @return
+     * @param body 新增入参
+     * @throws BaseException
+     * @throws IOException
      */
     @PostMapping("/insert")
-    public ResponseEntityWrapper insertNotice(@RequestBody NoticeBody body) throws BaseException {
+    public ResponseEntityWrapper insertNotice(@RequestBody NoticeBody body) throws BaseException, IOException {
         noticeService.insertNotice(body);
         return success();
     }
@@ -45,20 +48,27 @@ public class NoticeController extends BaseController {
      */
     @PostMapping("/delete")
     public ResponseEntityWrapper deleteNotice(@RequestParam String ids) throws BaseException {
+        if (StringUtils.isEmpty(ids)) {
+            throw new BaseException("公告id不能为空");
+        }
         noticeService.deleteNotice(ids);
         return success();
     }
 
     /**
-     * 查询web端公告分页
+     * Web端查询分页
      *
      * @param userId   用户id
+     * @param state    状态
+     * @param orgId    企业id
      * @param pageNo   当前页码
      * @param pageSize 每页显示的总条数
      * @return
+     * @throws BaseException
+     * @throws IOException
      */
     @PostMapping("/page")
-    public ResponseEntityWrapper selectNoticePage(@RequestParam String userId, Integer state, @RequestParam String orgId, @RequestParam Integer pageNo, @RequestParam Integer pageSize) throws BaseException {
+    public ResponseEntityWrapper selectNoticePage(@RequestParam String userId, Integer state, @RequestParam String orgId, @RequestParam Integer pageNo, @RequestParam Integer pageSize) throws BaseException, IOException {
         Map<String, Object> map = noticeService.selectNoticePage(userId, state, orgId, pageNo, pageSize);
         return success(map);
     }
