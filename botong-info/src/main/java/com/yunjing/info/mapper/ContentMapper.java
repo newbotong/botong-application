2ad763b1c72b6f2775df8a5ddba260937057bd3d
VDESKTOP\043 <liushujie@vv.cc>
2018-04-19 16:37:53 +0800

    资讯模块checkstyle 修复

--- botong-info/src/main/java/com/yunjing/info/mapper/ContentMapper.java
+++ botong-info/src/main/java/com/yunjing/info/mapper/ContentMapper.java
@@ -2,9 +2,13 @@ package com.yunjing.info.mapper;
 
 import com.common.mybatis.mapper.IBaseMapper;
 import com.yunjing.info.model.Content;
-import com.yunjing.info.model.ContentColumn;
 import org.springframework.stereotype.Repository;
 
+/**
+ * V1 资讯内容
+ * @author liushujie.
+ * @since 2018/3/23.
+ */
 @Repository
 public interface ContentMapper extends IBaseMapper<Content> {
 }
