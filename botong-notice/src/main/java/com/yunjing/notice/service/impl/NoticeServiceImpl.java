d29bd7af1640b50cbd1aeb7de580c5286b70e5b6
lishuangxi <lishuangxi@vv.cc>
2018-04-25 14:56:11 +0800

    update

--- botong-notice/src/main/java/com/yunjing/notice/service/impl/NoticeServiceImpl.java
+++ botong-notice/src/main/java/com/yunjing/notice/service/impl/NoticeServiceImpl.java
@@ -27,6 +27,7 @@ import com.yunjing.notice.processor.okhttp.OrgStructureService;
 import com.yunjing.notice.service.NoticeService;
 import com.yunjing.notice.service.NoticeUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
@@ -110,6 +111,7 @@ public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, NoticeEntity> i
             throw new BaseRuntimeException(StatusCode.ROLE_LIST_NOT_EXISTS.getStatusCode(),"该用户查询不到");
         }
         String[] passportIds = memberList.stream().map(Member::getPassportId).toArray(String[]::new);
+        List<String> newList = new ArrayList(new HashSet(Arrays.asList(passportIds)));
         String[] memberIds = memberList.stream().map(Member::getId).toArray(String[]::new);
 
         List<String> userInfoBodies = Arrays.asList(memberIds);
@@ -177,7 +179,7 @@ public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, NoticeEntity> i
         //构建发送公告参数
         AppPushParam pushParam = new AppPushParam();
         pushParam.setNotificationTitle("公告");
-        pushParam.setAlias(passportIds);
+        pushParam.setAlias(newList.toArray(new String[0]));
         pushParam.setTitle(noticeEntity.getTitle());
         pushParam.setMap(map);
         pushParam.setMsg("您有一条新公告，请注意查收！");
@@ -212,6 +214,10 @@ public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, NoticeEntity> i
                 }
                 userInfoModelList.add(userInfoModel);
             }
+            List<UserInfoModel> userList = new ArrayList<>();
+            if (CollectionUtils.isNotEmpty(userInfoModelList)){
+                userList = new ArrayList(new HashSet(userInfoModelList));
+            }
             if (null != result) {
                 if (null != result.getData()) {
                     List<Member> listMember = result.getData();
@@ -222,7 +228,7 @@ public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, NoticeEntity> i
             dangParam.setBizId(noticeEntity.getId());
             dangParam.setSendTelephone(noticeBody.getPhone());
             dangParam.setBizType(1);
-            dangParam.setReceiveBody(userInfoModelList);
+            dangParam.setReceiveBody(userList);
             dangParam.setDangType(1);
             dangParam.setRemindType(1);
             dangParam.setSendType(1);
