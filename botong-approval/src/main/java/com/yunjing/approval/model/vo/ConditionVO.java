91bb1a47cb593ef93962c0925a06d63167a3e582
016 <271990760@qq.com>
2018-05-03 19:11:52 +0800

    修改提交审批的bug

---
+++ botong-approval/src/main/java/com/yunjing/approval/model/vo/ConditionVO.java
@@ -0,0 +1,13 @@
+package com.yunjing.approval.model.vo;
+
+import lombok.Data;
+
+@Data
+public class ConditionVO {
+
+    private String field;
+
+    private String value;
+
+    private Integer type;
+}
